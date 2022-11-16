03b9ddc1184510f716a4c7f3e5f30935cba7775e
Zulekah Khan <71935408+zulkhan99@users.noreply.github.com>
2022-11-16 11:12:06 -0500

    Update AppTest.java

--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest
 {
 	WebDriver driver; 
 	WebDriverWait wait; 
-	String url = "http://localhost";
+  	String url = "http://my-apache-php-app";
 	String validEmail = "user@example.com";
 	String validPassword = "password1234";
 	String invalidEmail = "none@example.com";
