429cd6109523cf785b3134e46808ef3853cb34d7
Zulekah Khan <71935408+zulkhan99@users.noreply.github.com>
2022-11-16 11:10:40 -0500

    fix problems

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v c:\\...\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+docker run -d -p 5000:80 --name my-apache-php-app -v /var/jenkins_home/workspace/test4/src:/var/www/html --net test4net php:7.2-apache
 sleep 1
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost to see your PHP application in action.'
+echo 'Visit http://localhost:5000 to see your PHP application in action.'
 
